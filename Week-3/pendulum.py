696458f1a77344c9cba1ddc3d069eecb76351e1b
Uzux <ms5719@ic.ac.uk>
2019-10-22 10:36:21 +0100

    Completed pendulum.py

--- Week-3/pendulum.py
+++ Week-3/pendulum.py
@@ -2,20 +2,36 @@ import math as m
 
 def main():
     while True:
-        user_input = input("Input the length of the pendulum:")
-        type_check = isinstance(user_input, int)
-        print (type_check)
-        if type_check == True:
+        user_input = input("Input the length of the pendulum in meters:")
+        try:
+            val = int(user_input)
             break
+        except ValueError:
+            print("That's not an int")
 
-    length = int(user_input)
-    accel = int(9.81)
+    while True:
+        user_input = input("Input the acceleration of the system:")
+        try:
+            dyspeed = int(user_input)
+        except ValueError:
+            print("That's not an int")
+
+
+    length = val
+    accel = dyspeed
     perio = period(length, accel)
     print(perio)
 
 def period(length, accel):
-    t = 2*m.pi*m.sqrt(length/accel)
-    return t
+    if accel != 0:
+        t = 2*m.pi*m.sqrt(length/accel)
+        return t    
+    else:
+        print("Acceleration cannot be 0!")
+        raise ValueError
+        
+    
+    
 
 if __name__ == '__main__':
     main()
